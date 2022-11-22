7d53b60d0834df98c026de2175950bd073b405e5
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-22 15:10:43 -0700

    commit tree 7407854cee2f74baac551c730e2aa86bec08e88b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
+class Book
+  attr_accessor :author, :page_count, :genre, :title
+  
+
+  def initialize(title)
+    @title = title
+    
+  end
+
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+
+
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe
+
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand 
+
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+  def return_self
+    self.brand
+  end
+
+end
+
+
+
+binding.pry
